c624e83af5d9b6d362ced86fd65ed17155b0584d
Andy <russellandy98@gmail.com>
2019-09-30 18:01:23 -0400

    Fixed an issue on sorting

--- src/reducers/reducer_assignments.js
+++ src/reducers/reducer_assignments.js
@@ -15,13 +15,15 @@ export default function(state = initialState, action) {
 
         case 'COURSE_SELECTED':
             //Adds an assignment to a new array that contains everything state has
-            return [...state,action.payload].slice().sort(
-             (course1,course2) => {
-                 let course1Name = course1.professor.name.split(' ')
-                 let course2Name = course2.professor.name.split(' ')
-                 return (course1Name[2]).localeCompare(course2Name[2])
-             })   
-            ;    
+            if([...state,action.payload].length < 2){
+                return [...state,action.payload].slice().sort(
+                        (course1,course2) => {
+                            let course1Name = course1.professor.name.split(' ')
+                            let course2Name = course2.professor.name.split(' ')
+                            return (course1Name[2]).localeCompare(course2Name[2])
+             })
+            }   
+            return [...state,action.payload]  
         default:
             return state
     }
